deb15cc62923205a0508c083b516da4fa0ddcac5
anactg <163449405+anactg@users.noreply.github.com>
2024-04-02 22:05:47 -0300

    tp2.c

--- tp2.c
+++ tp2.c
@@ -1,10 +1,43 @@
-/* Arquivo MAIN que usa o TAD racionais */
+#include "racionais.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-/* coloque seus includes aqui */
+int main() {
+    srand(0); // Inicializa a semente randômica
 
-int main (){
+    int n, max;
+    printf("Digite um valor para n (0 < n < 100): ");
+    scanf("%d", &n);
+    printf("Digite um valor para max (0 < max < 30): ");
+    scanf("%d", &max);
 
-    /* coloque seu codido aqui */
+    for (int i = 1; i <= n; i++) {
+        printf("%d: ", i);
+
+        struct racional r1 = sorteia_r(max);
+        struct racional r2 = sorteia_r(max);
+
+        printf("%d/%d %d/%d ", numerador_r(r1), denominador_r(r1), numerador_r(r2), denominador_r(r2));
+
+        if (!valido_r(r1) || !valido_r(r2)) {
+            printf("NUMERO INVALIDO\n");
+            return 1;
+        }
+
+        struct racional soma, subtracao, multiplicacao, divisao;
+        soma_r(r1, r2, &soma);
+        subtrai_r(r1, r2, &subtracao);
+        multiplica_r(r1, r2, &multiplicacao);
+        if (!divide_r(r1, r2, &divisao)) {
+            printf("DIVISAO INVALIDA\n");
+            return 1;
+        }
+
+        printf("%d/%d %d/%d %d/%d %d/%d\n", numerador_r(soma), denominador_r(soma),
+               numerador_r(subtracao), denominador_r(subtracao),
+               numerador_r(multiplicacao), denominador_r(multiplicacao),
+               numerador_r(divisao), denominador_r(divisao));
+    }
 
     return 0;
 }
